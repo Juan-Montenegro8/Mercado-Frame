469da080329e5a1bf4b7a72ffe19989f905b6ab5
Juan <jj.monte2002@gmail.com>
2024-10-16 20:22:28 -0500

    first commit

---
+++ src/mercado/Mercado.java
@@ -0,0 +1,19 @@
+
+package mercado;
+
+
+
+public class Mercado {
+
+  
+    public static void main(String[] args) {
+
+        vista v=new vista();
+        
+        v.setVisible(true);
+        
+        
+        
+    }
+    
+}
